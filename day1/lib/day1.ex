3b9bed3fa75d5d6b152473c52540446e02256ca1
Aiman Josefsson <aiman@Aimans-MBP.office.lan>
2019-12-03 00:30:04 +0100

    day 1_2

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,27 +1,17 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for Day1.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day1.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
   def read do
     input = File.read!("lib/signal.txt")
     input
     |> String.split("\n")
     |> Enum.map(&(String.to_integer(&1)))
-    |> Enum.map(&(div(&1, 3) - 2))
+    |> Enum.map(&(additional(&1) - &1))
     |> Enum.sum
-    |> IO.inspect
+  end
+  def additional(fuel) do
+    if fuel > 0 do
+      fuel + additional(div(fuel, 3) - 2)
+    else
+      0
+    end
   end
 end
